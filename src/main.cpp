b19cf42a8a64c8308651c13206fcb4f070919658
unknown6656 <unknown84@gmx.de>
2021-04-04 09:44:18 +0200

    added window creation code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,68 @@
 #include <stdio.h>
 #include <iostream>
+#include <Windows.h>
+#include <WinUser.h>
+#include <gdiplusgraphics.h>
 
+
+int paint(const HWND hwnd)
+{
+    const Graphics* graphics = Graphics::FromHWND(hwnd);
+
+
+    return 0;
+}
 
 int main(const int argc, const char** argv)
 {
+    const wchar_t CLASS_NAME[] = L"Sample Window Class";
+    WNDCLASS wc = { };
 
-    return 0;
+    wc.hInstance = nullptr;
+    wc.lpszClassName = CLASS_NAME;
+    wc.lpfnWndProc = [](HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) -> LRESULT
+    {
+        switch (uMsg)
+        {
+            case WM_PAINT:
+                return paint(hwnd);
+            default:
+                return DefWindowProc(hwnd, uMsg, wParam, lParam);
+        }
+
+        return 0;
+    };
+
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindowEx(
+        0,                           // Optional window styles.
+        CLASS_NAME,                  // Window class
+        L"Learn to Program Windows", // Window text
+        WS_OVERLAPPEDWINDOW,         // Window style
+
+        // Size and position
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+        nullptr, // Parent window    
+        nullptr, // Menu
+        nullptr, // Instance handle
+        nullptr  // Additional application data
+    );
+
+    if (!hwnd)
+        return -1;
+
+    ShowWindow(hwnd, SW_SHOW);
+
+    MSG msg = { };
+    msg.wParam = 0;
+
+    while (IsWindow(hwnd) && GetMessage(&msg, nullptr, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return msg.wParam;
 }
