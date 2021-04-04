b259c5206e06ce010be9fb34430f9748cae70ca2
unknown6656 <unknown84@gmx.de>
2021-04-04 12:13:57 +0200

    added viewport init code

--- Program.cs
+++ Program.cs
@@ -10,6 +10,10 @@ namespace GolfGame
 {
     public static unsafe class Program
     {
+        private const int INIT_WINDOW_WIDTH = 1440;
+        private const int INIT_WINDOW_HEIGHT = 900;
+
+
         public static int Main(string[] argv)
         {
             try
@@ -19,12 +23,20 @@ namespace GolfGame
                 GLFW.WindowHint(WindowHintInt.ContextVersionMinor, 3);
                 GLFW.WindowHint(WindowHintOpenGlProfile.OpenGlProfile, OpenGlProfile.Core);
 
-                Window* window = GLFW.CreateWindow(1440, 900, "Golf Game", null, null);
+                Window* window = GLFW.CreateWindow(INIT_WINDOW_WIDTH, INIT_WINDOW_HEIGHT, "Golf Game", null, null);
 
                 if (window == null)
                     return -1;
 
                 GLFW.MakeContextCurrent(window);
+                GLFW.SetFramebufferSizeCallback(window, (hwnd, width, height) => GL.Viewport(0, 0, width, height));
+                GL.Viewport(0, 0, INIT_WINDOW_WIDTH, INIT_WINDOW_HEIGHT);
+
+                while (!GLFW.WindowShouldClose(window))
+                {
+                    GLFW.SwapBuffers(window);
+                    GLFW.PollEvents();
+                }
             }
             finally
             {
