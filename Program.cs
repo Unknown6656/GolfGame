234f75ce6ccfdb041371a1c451ccd1e02d7d2340
unknown6656 <unknown84@gmx.de>
2021-04-04 11:31:04 +0200

    updated entry point

--- Program.cs
+++ Program.cs
@@ -1,11 +1,34 @@
-﻿
+﻿using OpenGL;
+using GLFW;
+
+using gl = OpenGL.GL;
+using glfw = GLFW.Glfw;
+
+
 namespace GolfGame
 {
     public static class Program
     {
         public static int Main(string[] argv)
         {
+            try
+            {
+                glfw.Init();
+                glfw.WindowHint(Hint.ContextVersionMajor, 3);
+                glfw.WindowHint(Hint.ContextVersionMinor, 3);
+                glfw.WindowHint(Hint.OpenglProfile, Profile.Core);
 
+                Window window = glfw.CreateWindow(1440, 900, "Golf Game", Monitor.None, Window.None);
+
+                if (window == Window.None)
+                    return -1;
+
+                glfw.MakeContextCurrent(window);
+            }
+            finally
+            {
+                glfw.Terminate();
+            }
 
             return 0;
         }
