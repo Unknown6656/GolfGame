a39cdd980eb9c390c3169d68ed98f2e0f197ca17
unknown6656 <unknown84@gmx.de>
2021-04-05 18:57:15 +0200

    moved utility classes

--- GolfGame.cs
+++ GolfGame.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System;
 
 using OpenTK.Windowing.GraphicsLibraryFramework;
@@ -11,37 +12,12 @@ using OpenTK;
 using OpenTK.Mathematics;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.Common;
+
 using Unknown6656.Mathematics.Numerics;
-using System.Collections.Generic;
 using Unknown6656.Common;
-using System.IO;
 
 namespace GolfGame
 {
-    public unsafe record BufferObject(uint ID)
-        : IDisposable
-    {
-        public BufferObject SetData<T>(Span<T> values, bool clear = false)
-            where T : unmanaged
-        {
-            GL.BindBuffer(BufferTargetARB.ArrayBuffer, ID);
-            GL.BufferData(BufferTargetARB.ArrayBuffer, values, BufferUsageARB.StaticDraw);
-
-            if (clear)
-                GL.BindBuffer(BufferTargetARB.ArrayBuffer, 0);
-
-            return this;
-        }
-
-        public void Dispose()
-        {
-            GL.BindBuffer(BufferTargetARB.ArrayBuffer, 0);
-            GL.DeleteBuffer(ID);
-        }
-
-        public static BufferObject Create() => new(GL.GenBuffer());
-    }
-
     public sealed class GolfGame
         : GameWindow
     {
@@ -130,37 +106,6 @@ namespace GolfGame
         {
             using (GolfGame game = new())
                 game.Run();
-        }
-    }
-
-    internal static class Util
-    {
-        public static float NextFloat(this XorShift random, float scale) => random.NextFloat() * scale;
-    }
-
-    public sealed class Shader
-    {
-        public uint Handle { get; }
-        public string Path { get; }
-        public string SourceCode { get; }
-        public string? CompileLog { get; }
-        public ShaderType ShaderType { get; }
-
-
-        public Shader(string path, ShaderType type)
-        {
-            Path = path;
-            SourceCode = File.ReadAllText(path);
-            Handle = GL.CreateShader(type);
-
-            GL.ShaderSource(Handle, SourceCode);
-            GL.CompileShader(Handle);
-
-            int log_length = 0;
-            string log = GL.GetShaderInfoLog(Handle, 1024 * 1024, ref log_length);
-
-            if (!string.IsNullOrEmpty(log))
-                CompileLog = log;
         }
     }
 
---
+++ Util.cs
@@ -0,0 +1,64 @@
+﻿using System.IO;
+using System;
+
+using OpenTK.Graphics.OpenGL;
+
+using Unknown6656.Mathematics.Numerics;
+
+namespace GolfGame
+{
+    public unsafe record BufferObject(uint ID)
+        : IDisposable
+    {
+        public BufferObject SetData<T>(Span<T> values, bool clear = false)
+            where T : unmanaged
+        {
+            GL.BindBuffer(BufferTargetARB.ArrayBuffer, ID);
+            GL.BufferData(BufferTargetARB.ArrayBuffer, values, BufferUsageARB.StaticDraw);
+
+            if (clear)
+                GL.BindBuffer(BufferTargetARB.ArrayBuffer, 0);
+
+            return this;
+        }
+
+        public void Dispose()
+        {
+            GL.BindBuffer(BufferTargetARB.ArrayBuffer, 0);
+            GL.DeleteBuffer(ID);
+        }
+
+        public static BufferObject Create() => new(GL.GenBuffer());
+    }
+
+    public sealed class Shader
+    {
+        public uint Handle { get; }
+        public string Path { get; }
+        public string SourceCode { get; }
+        public string? CompileLog { get; }
+        public ShaderType ShaderType { get; }
+
+
+        public Shader(string path, ShaderType type)
+        {
+            Path = path;
+            SourceCode = File.ReadAllText(path);
+            Handle = GL.CreateShader(type);
+
+            GL.ShaderSource(Handle, SourceCode);
+            GL.CompileShader(Handle);
+
+            int log_length = 0;
+            string log = GL.GetShaderInfoLog(Handle, 1024 * 1024, ref log_length);
+
+            if (!string.IsNullOrEmpty(log))
+                CompileLog = log;
+        }
+    }
+
+    internal static class Util
+    {
+        public static float NextFloat(this XorShift random, float scale) => random.NextFloat() * scale;
+    }
+}
