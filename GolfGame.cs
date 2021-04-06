698528ec8fec7eb7186347143de6abccef99f811
unknown6656 <unknown84@gmx.de>
2021-04-06 10:36:49 +0200

    began using EBOs

--- GolfGame.cs
+++ GolfGame.cs
@@ -15,6 +15,12 @@ using OpenTK.Windowing.Common;
 
 using Unknown6656.Mathematics.Numerics;
 using Unknown6656.Common;
+using System.Threading.Tasks.Sources;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Runtime.ExceptionServices;
 
 namespace GolfGame
 {
@@ -25,7 +31,11 @@ namespace GolfGame
         private const int INIT_WINDOW_HEIGHT = 900;
 
         private static GolfCourse? _curent_golf_course = new(Par.Par4, 420);
-        private readonly List<BufferObject> _vbos = new();
+        private readonly Dictionary<string, (int Location, UniformType Type)> _uniforms = new();
+        private readonly Dictionary<string, (uint VBO, uint VAO, uint EBO, uint attribute, Array vertices, Array indices)> _attributes = new();
+        private bool _loaded = true;
+
+        public uint ProgramHandle { get; private set; }
 
 
         public GolfGame()
@@ -37,7 +47,7 @@ namespace GolfGame
                 },
                 new NativeWindowSettings {
                     API = ContextAPI.OpenGL,
-                    APIVersion = new(4, 6),
+                    APIVersion = new(3,3),
                     Size = new(INIT_WINDOW_WIDTH, INIT_WINDOW_HEIGHT),
                     NumberOfSamples = 4,
                     Title = "Golf Game",
@@ -46,29 +56,138 @@ namespace GolfGame
 #endif
                 })
         {
+            unsafe
+            {
+                GLFW.MakeContextCurrent(WindowPtr);
+            }
+
+            Console.WriteLine(GL.GetString(StringName.Renderer));
+            Console.WriteLine(GL.GetString(StringName.Version));
+            Console.WriteLine(GL.GetString(StringName.ShadingLanguageVersion));
+            Console.WriteLine(GL.GetString(StringName.Vendor));
+
+            GL.Enable(EnableCap.DebugOutput);
+
+            Task.Factory.StartNew(async delegate
+            {
+                try
+                {
+                    while (_loaded)
+                    {
+                        uint uid = 0;
+                        int length = -1;
+                        DebugType type = default;
+                        DebugSource source = default;
+                        DebugSeverity severity = default;
+                        uint count = GL.GetDebugMessageLog(1, 1024, ref source, ref type, ref uid, ref severity, ref length, out string message);
+
+                        if (count > 0 || length >= 0)
+                            Console.WriteLine($"{uid:x8}: {source} {type} {severity} | {GL.GetError()} | {source}");
+                        else
+                            await Task.Delay(50);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            });
         }
 
-        protected override void OnLoad()
+        protected override unsafe void OnLoad()
         {
-            uint program = GL.CreateProgram();
+            GL.Enable(EnableCap.VertexArray);
+
+            ProgramHandle = GL.CreateProgram();
+
             using Shader s_vert = new("shaders/shader.vert", ShaderType.VertexShader);
             using Shader s_frag = new("shaders/shader.frag", ShaderType.FragmentShader);
+            int uniform_count;
 
-            GL.AttachShader(program, s_vert);
-            GL.AttachShader(program, s_frag);
-            GL.LinkProgram(program);
-            GL.UseProgram(program);
-            GL.DetachShader(program, s_vert);
-            GL.DetachShader(program, s_frag);
+            Console.WriteLine(s_vert.CompileLog);
+            Console.WriteLine(s_frag.CompileLog);
+
+            GL.AttachShader(ProgramHandle, s_vert);
+            GL.AttachShader(ProgramHandle, s_frag);
+            GL.LinkProgram(ProgramHandle);
+            GL.UseProgram(ProgramHandle);
+            GL.DetachShader(ProgramHandle, s_vert);
+            GL.DetachShader(ProgramHandle, s_frag);
+            GL.GetProgramiv(ProgramHandle, ProgramPropertyARB.ActiveUniforms, &uniform_count);
+
+            for (uint i = 0; i < uniform_count; i++)
+            {
+                UniformType type = default;
+                int length = 0, size = 0;
+                string key = GL.GetActiveUniform(ProgramHandle, i, 1024, ref length, ref size, ref type);
+                int location = GL.GetUniformLocation(ProgramHandle, key);
+
+                _uniforms[key] = (location, type);
+            }
+
             GL.ClearColor(.3f, .5f, 1f, 1f);
+
+
+            (Vector3[] vertices, uint[] indices) = _curent_golf_course.Rasterize(1);
+
+            ProvideVBO("aPosition", vertices, indices);
 
             base.OnLoad();
         }
 
+        private unsafe (uint VBO, uint VAO, uint EBO, uint attribute) ProvideVBO<T>(string name, T[] array, uint[] indices)
+            where T : unmanaged
+        {
+            uint vbo, vao = 0, ebo = 0, attribute;
+
+            if (_attributes.TryGetValue(name, out var entry))
+                (vbo, vao, ebo, attribute) = (entry.VBO, entry.VAO, entry.EBO, entry.attribute);
+            else
+            {
+                attribute = (uint)GL.GetAttribLocation(ProgramHandle, name);
+                vbo = GL.GenBuffer();
+            }
+
+            GL.BindBuffer(BufferTargetARB.ArrayBuffer, vbo);
+            GL.BufferData(BufferTargetARB.ArrayBuffer, array, BufferUsageARB.StaticDraw); // DynamicDraw
+
+            if (vao == 0)
+                vao = GL.GenVertexArray();
+
+            GL.BindVertexArray(vao);
+            GL.VertexAttribPointer(attribute, sizeof(T) / sizeof(float), VertexAttribPointerType.Float, false, sizeof(T), null);
+            GL.EnableVertexAttribArray(attribute);
+
+            if (ebo == 0)
+                ebo = GL.GenBuffer();
+
+            GL.BindBuffer(BufferTargetARB.ElementArrayBuffer, ebo);
+            GL.BufferData(BufferTargetARB.ElementArrayBuffer, indices, BufferUsageARB.StaticDraw);
+            GL.ObjectLabel(ObjectIdentifier.Buffer, vbo, name.Length + 4, name + "-vbo");
+            GL.ObjectLabel(ObjectIdentifier.VertexArray, vao, name.Length + 4, name + "-vao");
+            GL.ObjectLabel(ObjectIdentifier.Buffer, ebo, name.Length + 4, name + "-ebo");
+
+            _attributes[name] = (vbo, vao, ebo, attribute, array, indices);
+
+            return (vbo, vao, ebo, attribute);
+        }
+
         protected override void OnUnload()
         {
-            _vbos.Do(obj => obj.Dispose());
-            _vbos.Clear();
+            GL.UseProgram(0);
+
+            foreach ((uint vbo, uint vao, uint ebo, _, _, _) in _attributes.Values)
+            {
+                GL.BindBuffer(BufferTargetARB.ArrayBuffer, 0);
+                GL.BindVertexArray(0);
+                GL.DeleteBuffer(vbo);
+                GL.DeleteBuffer(ebo);
+                GL.DeleteVertexArray(vao);
+            }
+
+            GL.DeleteProgram(ProgramHandle);
+
+            _attributes.Clear();
 
             base.OnUnload();
         }
@@ -77,15 +196,17 @@ namespace GolfGame
         {
             GLFW.Terminate();
 
+            _loaded = false;
+
             base.OnClosed();
         }
 
-        //protected override void OnResize()
-        //{
-        //    GL.Viewport(0, 0, Width, Height);
-        //
-        //    base.OnResize();
-        //}
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            GL.Viewport(0, 0, Size.X, Size.Y);
+
+            base.OnResize(e);
+        }
 
         protected override void OnKeyUp(KeyboardKeyEventArgs e)
         {
@@ -108,6 +229,16 @@ namespace GolfGame
         private void RenderImage()
         {
             GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.UseProgram(ProgramHandle);
+
+            foreach ((_, (uint vbo, uint vao, uint ebo, _, Array vertices, Array indices)) in _attributes)
+            {
+                //GL.BindBuffer(BufferTargetARB.ArrayBuffer, vbo);
+                //GL.BindBuffer(BufferTargetARB.ElementArrayBuffer, ebo);
+                GL.BindVertexArray(vao);
+                //GL.DrawArrays(PrimitiveType.Triangles, 0, vertices.Length);
+                GL.DrawElements(PrimitiveType.Triangles, indices.Length, DrawElementsType.UnsignedInt, 0);
+            }
 
             // TODO
         }
@@ -170,32 +301,34 @@ namespace GolfGame
                 CourseMidwayPoints = Array.Empty<Vector2>();
         }
 
-        public GolfCourseVectorBuffer Rasterize()
+        public (Vector3[] vertices, uint[] indices) Rasterize(int size)
         {
-            GolfCourseVectorBuffer vbo = new()
+            Vector3[] vertices = new Vector3[(size + 1) * (size + 1)];
+            uint[] indices = new uint[size * size * 6];
+
+            for (int i = 0; i < vertices.Length; ++i)
             {
-                VBOIndex = GL.GenBuffer(),
-            };
+                int ix = i % (size + 1);
+                int iz = i / (size + 1);
+                float x = (float)ix / size;
+                float z = (float)iz / size;
+                float y = 0f;
 
-            GL.BindBuffer(BufferTargetARB.ArrayBuffer, vbo.VBOIndex);
-            GL.BufferData(BufferTargetARB.ArrayBuffer, vbo.Vertices, BufferUsageARB.StaticDraw); // DynamicDraw
-            GL.BindBuffer(BufferTargetARB.ArrayBuffer, 0);
+                vertices[i] = new(x, z, y);
 
+                if (ix < size && iz < size)
+                {
+                    indices[(ix + iz * size) * 6 + 0] = (uint)(ix + iz * (size + 1));
+                    indices[(ix + iz * size) * 6 + 1] =
+                    indices[(ix + iz * size) * 6 + 3] = (uint)(ix + 1 + iz * (size + 1));
+                    indices[(ix + iz * size) * 6 + 4] = (uint)(ix + 1 + (iz + 1) * (size + 1));
+                    indices[(ix + iz * size) * 6 + 2] =
+                    indices[(ix + iz * size) * 6 + 5] = (uint)(ix + (iz + 1) * (size + 1));
+                }
+            }
 
-            GL.Enable(EnableCap.VertexArray);
-            GL.BindBuffer(BufferTargetARB.ArrayBuffer, vbo.VBOIndex);
-            //GL.vertexpointer
-
-
-            return vbo;
+            return (vertices, indices);
         }
-    }
-
-    public sealed class GolfCourseVectorBuffer
-    {
-        public uint VBOIndex { get; init; }
-        public Vector2[] Vertices { get; init; }
-        public (int i1, int i2, int i3)[] Indices { get; init; }
     }
 
     public enum Par
