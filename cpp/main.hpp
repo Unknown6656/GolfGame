2680bfa5439bf02c7beafbce73c3eae912d97cb2
unknown6656 <unknown84@gmx.de>
2021-04-09 22:45:03 +0200

    added includes

--- cpp/main.hpp
+++ cpp/main.hpp
@@ -7,14 +7,23 @@
 #define INIT_HEIGHT 900
 
 #include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <windows.h>
 
+//#include <gl.h>
 #include <GL/glew.h>
+#include <GL/wglew.h>
+// #include <GL/glu.h>
 #include <GLFW/glfw3.h>
+
 
 #include "game.hpp"
 
 
 int __cdecl main(const int, const char** const);
+
+unsigned int compile_shader(const std::string, const GLenum);
 
 int window_load(GLFWwindow* const);
 void window_render(GLFWwindow* const);
