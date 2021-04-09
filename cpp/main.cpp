15e6f4d20b67dcc31fa65ca2686977928b602fbe
unknown6656 <unknown84@gmx.de>
2021-04-09 22:45:15 +0200

    added shader compiler function

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -35,6 +35,36 @@ int __cdecl main(const int argc, const char** const argv)
     return exit_code;
 }
 
+unsigned int compile_shader(const std::string path, const GLenum type)
+{
+    std::ifstream stream(path);
+    std::string source(
+        (std::istreambuf_iterator<char>(stream)),
+        (std::istreambuf_iterator<char>())
+    );
+    const char* c_source = source.c_str();
+    const unsigned int shader = glCreateShader(type);
+
+    glShaderSource(shader, 1, &c_source, NULL);
+    glCompileShader(shader);
+
+    int success;
+    char log[1024];
+
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+
+    if (!success)
+    {
+        glGetShaderInfoLog(shader, 1024, NULL, log);
+
+        std::cout << "failed to compile shader:" << std::endl << log << std::endl;
+
+        return 0;
+    }
+    else
+        return shader;
+}
+
 int window_load(GLFWwindow* const window)
 {
     glfwMakeContextCurrent(window);
@@ -44,6 +74,32 @@ int window_load(GLFWwindow* const window)
         glViewport(0, 0, width, height);
     });
 
+    const unsigned int program = glCreateProgram();
+    const unsigned int vert_shader = compile_shader("shader.vert", GL_VERTEX_SHADER);
+    const unsigned int frag_shader = compile_shader("shader.frag", GL_FRAGMENT_SHADER);
+
+    if (!vert_shader && !frag_shader)
+        return -1;
+
+    glAttachShader(program, vert_shader);
+    glAttachShader(program, frag_shader);
+    glLinkProgram(program);
+    glUseProgram(program);
+    glDeleteShader(vert_shader);
+    glDeleteShader(frag_shader);
+
+
+    const float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+    unsigned int vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+
 
     return 0;
 }
@@ -52,6 +108,11 @@ void window_render(GLFWwindow* const)
 {
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+
+
+    
+
+
 }
 
 void window_process_input(GLFWwindow* const window)
