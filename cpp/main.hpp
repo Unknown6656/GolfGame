f6b60cd59b0d6554dfe842283e5022de9f4f173a
unknown6656 <unknown84@gmx.de>
2021-04-10 11:11:13 +0200

    updated inlcudes to use glad

--- cpp/main.hpp
+++ cpp/main.hpp
@@ -11,12 +11,12 @@
 #include <cstdio>
 #include <windows.h>
 
-//#include <gl.h>
-#include <GL/glew.h>
-#include <GL/wglew.h>
+// #include <GL/glew.h>
+// #include <GL/wglew.h>
 // #include <GL/glu.h>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
+//#include <KHR/khrplatform.h>
 
 #include "game.hpp"
 
@@ -24,7 +24,9 @@
 int __cdecl main(const int, const char** const);
 
 unsigned int compile_shader(const std::string, const GLenum);
+int get_attribute_location(const std::string);
 
 int window_load(GLFWwindow* const);
+void window_unload(GLFWwindow* const);
 void window_render(GLFWwindow* const);
 void window_process_input(GLFWwindow* const);
