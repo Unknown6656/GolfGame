77162b72b08bcd4376e72aa4e228c8e4a810cda8
unknown6656 <unknown84@gmx.de>
2021-04-13 21:53:22 +0200

    added a few common functions

--- cpp/shaders/common.glsl
+++ cpp/shaders/common.glsl
@@ -2,6 +2,16 @@
 
 precision highp float;
 
+#define PI 3.14159265358979323846264338327950288419716939937
+#define TAU 6.28318530717958647692528676655900576839433879875
+#define EPS 0.00001
+#define PAR3 0
+#define PAR4 1
+#define PAR5 2
+
+#ifndef saturate
+    #define saturate(v) clamp(v, 0., 1.)
+#endif
 
 uniform float u_time;
 uniform mat4 u_model;
@@ -38,13 +48,24 @@ float random(in vec2 position)
 
 float noise2D(in vec2 position)
 {
-    vec2 i = floor(position);
-    vec2 f = fract(position);
-    float a = random(i);
-    float b = random(i + vec2(1.0, 0.0));
-    float c = random(i + vec2(0.0, 1.0));
-    float d = random(i + vec2(1.0, 1.0));
-    vec2 u = smoothstep(0.0, 1.0, f);
+    const vec2 i = floor(position);
+    const vec2 f = fract(position);
+    const float a = random(i);
+    const float b = random(i + vec2(1.0, 0.0));
+    const float c = random(i + vec2(0.0, 1.0));
+    const float d = random(i + vec2(1.0, 1.0));
+    const vec2 u = smoothstep(0.0, 1.0, f);
 
     return mix(a, b, u.x) + (c - a)* u.y * (1.0 - u.x) + (d - b) * u.x * u.y;
+}
+
+vec3 hue2rgb(float hue)
+{
+    hue = fract(hue);
+
+    return saturate(vec3(
+        abs(hue * 6. - 3.) - 1.,
+        2. - abs(hue * 6. - 2.),
+        2. - abs(hue * 6. - 4.)
+    ));
 }
