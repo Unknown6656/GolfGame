f45589641723acf163ae0cce0e08f003359b8295
unknown6656 <unknown84@gmx.de>
2021-04-13 15:36:46 +0200

    added regex functions with callback

--- cpp/common.hpp
+++ cpp/common.hpp
@@ -15,6 +15,7 @@
 #include <sstream>
 #include <fstream>
 #include <string>
+#include <regex>
 #include <cstdio>
 #include <windows.h>
 #include <vector>
@@ -59,4 +60,48 @@ inline glm::vec4 from_argb(const unsigned int argb)
 inline glm::vec3 from_rgb(unsigned int rgb) noexcept
 {
     return from_argb(rgb & 0x00ffffff).xyz();
+}
+
+
+namespace std
+{
+    template<class BidirIt, class Traits, class CharT, class UnaryFunction>
+    std::basic_string<CharT> regex_replace(BidirIt first, BidirIt last, const std::basic_regex<CharT, Traits>& re, UnaryFunction f)
+    {
+        std::basic_string<CharT> s;
+        typename std::match_results<BidirIt>::difference_type positionOfLastMatch = 0;
+        auto endOfLastMatch = first;
+
+        auto callback = [&](const std::match_results<BidirIt>& match)
+        {
+            auto positionOfThisMatch = match.position(0);
+            auto diff = positionOfThisMatch - positionOfLastMatch;
+            auto startOfThisMatch = endOfLastMatch;
+
+            std::advance(startOfThisMatch, diff);
+
+            s.append(endOfLastMatch, startOfThisMatch);
+            s.append(f(match));
+
+            auto lengthOfMatch = match.length(0);
+
+            positionOfLastMatch = positionOfThisMatch + lengthOfMatch;
+            endOfLastMatch = startOfThisMatch;
+
+            std::advance(endOfLastMatch, lengthOfMatch);
+        };
+
+        std::regex_iterator<BidirIt> begin(first, last, re), end;
+        std::for_each(begin, end, callback);
+
+        s.append(endOfLastMatch, last);
+
+        return s;
+    }
+
+    template<class Traits, class CharT, class UnaryFunction>
+    std::string regex_replace(const std::string& s, const std::basic_regex<CharT, Traits>& re, UnaryFunction f)
+    {
+        return regex_replace(s.cbegin(), s.cend(), re, f);
+    }
 }
