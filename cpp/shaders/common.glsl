c2e5555351057d96755e51a92de22c602cb2094e
unknown6656 <unknown84@gmx.de>
2021-04-18 20:10:31 +0200

    fixed a few include bugs

--- cpp/shaders/common.glsl
+++ cpp/shaders/common.glsl
@@ -29,3 +29,47 @@ precision highp float;
 
 
 uniform float u_time;
+
+
+
+float random(in vec2 position)
+{
+    return fract(sin(dot(position, vec2(12.9898, 78.233))) * 43758.5453123);
+}
+
+float random(in vec3 position)
+{
+    position = fract(position * .3183099 + .1) * 17;
+
+    return fract(position.x * position.y * position.z * (position.x + position.y + position.z));
+}
+
+float noise3D(in vec3 position)
+{
+    vec3 i = floor(position);
+    vec3 f = fract(position);
+
+    f *= f * (3 - 2 * f);
+
+    return mix(mix(mix(random(i + vec3(0, 0, 0)), 
+                       random(i + vec3(1, 0, 0)), f.x),
+                   mix(random(i + vec3(0, 1, 0)), 
+                       random(i + vec3(1, 1, 0)), f.x), f.y),
+               mix(mix(random(i + vec3(0, 0, 1)), 
+                       random(i + vec3(1, 0, 1)), f.x),
+                   mix(random(i + vec3(0, 1, 1)), 
+                       random(i + vec3(1, 1, 1)), f.x), f.y), f.z);
+}
+
+float noise2D(in vec2 position)
+{
+    const vec2 i = floor(position);
+    const vec2 f = fract(position);
+    const float a = random(i);
+    const float b = random(i + vec2(1.0, 0.0));
+    const float c = random(i + vec2(0.0, 1.0));
+    const float d = random(i + vec2(1.0, 1.0));
+    const vec2 u = smoothstep(0.0, 1.0, f);
+
+    return mix(a, b, u.x) + (c - a)* u.y * (1.0 - u.x) + (d - b) * u.x * u.y;
+}
